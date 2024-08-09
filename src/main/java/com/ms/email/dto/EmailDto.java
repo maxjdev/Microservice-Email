aec12eaf31688a36837a1c75a576be3be85912c2
maxjdev <maxjramosdev@gmail.com>
2024-08-08 22:42:09 -0300

    changes class to record

--- src/main/java/com/ms/email/dto/EmailDto.java
+++ src/main/java/com/ms/email/dto/EmailDto.java
@@ -2,20 +2,19 @@ package com.ms.email.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
-@Data
-public class EmailDto {
-    @NotBlank
-    private String ownerRef;
-    @NotBlank
-    @Email
-    private String emailFrom;
-    @NotBlank
-    @Email
-    private String emailTo;
-    @NotBlank
-    private String subject;
-    @NotBlank
-    private String text;
+public record EmailDto(
+        @NotBlank
+        String ownerRef,
+        @NotBlank
+        @Email
+        String emailFrom,
+        @NotBlank
+        @Email
+        String emailTo,
+        @NotBlank
+        String subject,
+        @NotBlank
+        String text
+) {
 }
