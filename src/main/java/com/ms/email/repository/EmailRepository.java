b59abc7707770d9a12409cb6837b87ebb448cf19
maxjdev <maxjramosdev@gmail.com>
2024-08-10 19:08:05 -0300

    change extended interface in repository

--- src/main/java/com/ms/email/repository/EmailRepository.java
+++ src/main/java/com/ms/email/repository/EmailRepository.java
@@ -1,11 +1,11 @@
 package com.ms.email.repository;
 
 import com.ms.email.model.Email;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface EmailRepository extends CrudRepository<Email, UUID> {
+public interface EmailRepository extends JpaRepository<Email, UUID> {
 }
